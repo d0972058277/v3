a08e5177160ac718753874ba81e6dfd8d66a6b81
PorriDG3 <gn02053231@gmail.com>
2020-09-25 15:03:32 +0800

    add auto pack and fix warning

--- V3Lib/Commands/Abstractions/ICommandWithResult.cs
+++ V3Lib/Commands/Abstractions/ICommandWithResult.cs
@@ -5,6 +5,6 @@ namespace V3Lib.Commands.Abstractions
     public interface ICommandWithResult<T> : ICommand
     {
         T Result { get; }
-        Task<T> ExecuteAsync();
+        new Task<T> ExecuteAsync();
     }
 }
